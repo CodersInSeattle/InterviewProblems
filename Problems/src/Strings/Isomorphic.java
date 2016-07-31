3dc17c4461a8bac637329ecc877828576fd71244
Su <sujack@amazon.com>
2016-07-31 15:20:55 -0700

    Added comment about the problem

--- Problems/src/Strings/Isomorphic.java
+++ Problems/src/Strings/Isomorphic.java
@@ -1,3 +1,26 @@
+/**
+ 205. Isomorphic Strings
+
+    Total Accepted: 67129
+    Total Submissions: 217571
+    Difficulty: Easy
+
+	Given two strings s and t, determine if they are isomorphic.
+
+	Two strings are isomorphic if the characters in s can be replaced to get t.
+
+	All occurrences of a character must be replaced with another character while preserving the order of characters. No two characters may map to the same character but a character may map to itself.
+
+	For example,
+	Given "egg", "add", return true.
+
+	Given "foo", "bar", return false.
+
+	Given "paper", "title", return true.
+
+	Note:
+	You may assume both s and t have the same length.
+*/
 public class Solution {
     public boolean isIsomorphic(String s, String t) {
         if (s == null && t == null) {
--- Problems/src/Strings/LongestSubStringWithKDistinctChar.java
+++ Problems/src/Strings/LongestSubStringWithKDistinctChar.java
@@ -1,3 +1,12 @@
+/**
+ 340. Longest Substring with At Most K Distinct Characters
+
+ Given a string, find the length of the longest substring T that contains at most k distinct characters.
+
+	For example, Given s = “eceba” and k = 2,
+
+	T is "ece" which its length is 3. 
+ */
 import java.util.*;
 
 public class Solution {
