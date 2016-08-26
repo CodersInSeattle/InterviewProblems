9c5ee06bb7a19689d026514948932d2f9347f063
stonemary <stonary.henary@gmail.com>
2016-08-25 19:46:17 -0700

    More bit problems - power of 2/4 - is_parse - get_profession

--- Problems/src/Bit/bit.py
+++ Problems/src/Bit/bit.py
@@ -1,19 +1,171 @@
 """
-Problem:
-    Given an integer, find the number of bits set. 
+bit operations: ~, &, |, ^, >>, <<
+
+Python 3 warning - this file is in python 3...
+
+Problem 1:
+    Given an integer, find the number of bits set.
+
+Problem 2:
+    Check if a given number is sparse or not
+    A number is said to be a sparse number if in binary representation of the number no two or more consecutive bits are set. Write a function to check if a given number is Sparse or not.
+
+    example:
+    01010101 ==> sparse
+    000001   ==> sparse
+    001100   ==> not sparse
+
+Problem 3:
+    http://www.geeksforgeeks.org/find-profession-in-a-hypothetical-special-situation/
+
+    Consider a special family of Engineers and Doctors with following rules :
+    1. Everybody has two children.
+    2. First child of an Engineer is an Engineer and second child is a Doctor.
+    3. First child of an Doctor is Doctor and second child is an Engineer.
+    4. All generations of Doctors and Engineers start with Engineer.
+
+    We can represent the situation using below diagram:
+                E
+           /        \
+          E          D
+        /   \       /  \
+       E     D     D    E
+      / \   / \   / \   / \
+     E   D D   E  D  E  E  D
+
+    Given level and position of a person in above ancestor tree, find profession of the person.
+
+    Examples:
+
+    Input : level = 4, pos = 2
+    Output : Doctor
+
+    Input : level = 3, pos = 4
+    Output : Engineer
+
+    1
+    10
+    1001
+    10010110
+
 """
+
 
 def bit_count(x):
     """
-    Given an integer x, return the number of bits set in the integer.
+    Problem 1:
+        Given an integer x, return the number of bits set in the integer.
+
+    ----------
+    explanation:
+        x - 1 => flips all bits in x up to the most right set bit
+        x & (x - 1): remove the last set bit for x.
+
+        therefore, repetitively set x = x & (x - 1), until x = 0, allows calculation of number of set bits in x.
+
+    example:
+        x     = 1010
+        x - 1 = 1001
+        &     = 1000 <== new x
+
+        x     = 1000
+        x - 1 = 0111
+        &     = 0000 <=== new x
+
+        new x ==> 0
+        number of iteration ==> 2
+        number of bits set in x ==> 2
+    -----------
+    does it work for negative numbers?
+
     """
     if x < 0:
         x = -x
 
     count = 0
+    # get rid of one 1 at a time
     while x != 0:
-        x = x & (x - 1)
+        x &= (x - 1)
         count += 1
     return count
 
 
+def is_power_of_two(x):
+    """
+    Return true if x is power of 2.
+    ------------------
+    explanation:
+        2 -> 10
+        4 -> 100
+        6 -> 1000
+
+        power of 2 ==> 1...0
+
+        power of 2 - 1 ==> 0...1
+
+        & ==> 0
+    """
+    return int(x) == x and x >= 1 and x & (x - 1) == 0
+
+
+def is_power_of_four(x):
+    """
+    Usually a follow up of `is_power_of_two`.
+
+    Return true if x is power of 4.
+    -------
+        1  ==> 1
+        4  ==> 100
+        16 ==> 10000
+        64 ==> 1000000
+        ....
+
+        not only has to be 1...0, but the 1 must be on the odd position.
+
+        ==> mask with 1010101010..etc.
+        how?
+    """
+    return int(x) == x and x > 1 and x & (x - 1) == 0 and x & 0x55555555 != 0
+
+
+def is_sparse(x):
+    """
+    Given a number x, returns true if it is sparse and false otherwise.
+    -----
+    explanation:
+
+    if there is an `11` in a number, shift one digit and there will still be an 1
+    overlapping.
+    x       = 0110000
+    x >> 1  = 0011000
+                ^ overlaps
+    &       = 0010000  ==> not zero.
+    """
+    if x < 0:
+        x = -x
+    return (x & (x >> 1)) == 0
+
+
+def get_profession(level, position):
+    """
+    Given the level and position of the person from the special family (see Problem 3), returns the profession of the
+    person.
+    :return: 'E' if he/she is an engineer and 'D' if he/she is a doctor.
+    """
+
+    if level < 1:
+        return ValueError('Level must be bigger than 0')
+    if not 0 < position <= 2 ** (level - 1):
+        return ValueError('Invalid position: {}'.format(position))
+
+    # 1 for engineer
+    generation = 1
+    for i in range(0, level - 1):
+        number_of_bits = 2 ** i
+        generation = (generation >> number_of_bits) + ~generation
+
+    # find out the position'th bit is set or not
+    profession = (generation >> (generation.bit_length() - position)) % 2
+    if profession == '1':
+        return 'E'
+    return 'D'
--- Problems/src/Bit/test_bit.py
+++ Problems/src/Bit/test_bit.py
@@ -1,23 +1,125 @@
 import random
+import math
 
-import bit
+from Problems.src.Bit import bit
 
 
-class TestBit(object):
-    
-    def _bit_count(self, x):
+RANDOM_TEST_ITERATION = 10
+
+
+class TestBitCount(object):
+
+    @staticmethod
+    def _bit_count_naive(x):
         return bin(x).count('1')
 
     def test_bit_count_zero(self):
-        assert self._bit_count(0) == bit.bit_count(0)
+        assert bit.bit_count(0) == self._bit_count_naive(0) == 0
 
     def test_bit_count_negative(self):
-        assert self._bit_count(-20) == bit.bit_count(-20) 
+        assert bit.bit_count(-20) == self._bit_count_naive(-20) == 2   # '-0b10100'
 
     def test_bit_count_positive(self):
-        assert self._bit_count(90) == bit.bit_count(90)
+        assert bit.bit_count(90) == self._bit_count_naive(90) == 4  # '0b1011010'
 
     def test_bit_count_random(self):
-        random_int = random.randint(-9999999, 9999999)
-        assert self._bit_count(random_int) == bit.bit_count(random_int)
 
+        for i in range(RANDOM_TEST_ITERATION):
+            random_int = random.randint(-9999999, 9999999)
+            assert bit.bit_count(random_int) == self._bit_count_naive(random_int), \
+                'failed to calculate bit set for {}'.format(random_int)
+
+
+class TestIsSparse(object):
+
+    @staticmethod
+    def _is_sparse_naive(x):
+        return not ('11' in bin(x))
+
+    def test_is_sparse_zero(self):
+        assert bit.is_sparse(0) == self._is_sparse_naive(0) == True
+
+    def test_is_sparse_negative(self):
+        assert bit.is_sparse(-1) == self._is_sparse_naive(-1) == True
+        assert bit.is_sparse(-3) == self._is_sparse_naive(-3) == False
+
+    def test_is_sparse_positive(self):
+        assert bit.is_sparse(1) == self._is_sparse_naive(1) == True
+        assert bit.is_sparse(3) == self._is_sparse_naive(3) == False
+
+    def test_is_sparse_random(self):
+        for i in range(RANDOM_TEST_ITERATION):
+            random_int = random.randint(-99999999, 99999999)
+            assert bit.is_sparse(random_int) == self._is_sparse_naive(random_int), \
+                'failed to calculate bit set for {}'.format(random_int)
+
+
+class TestIsPowerOfTwo(object):
+
+    @staticmethod
+    def _is_power_of_two_naive(x):
+        if x < 0 or int(x) != x:
+            return False
+        power = math.log(x, 2)
+        return power == int(power)
+
+    def test_is_power_of_two_negative(self):
+        assert not bit.is_power_of_two(-1)
+
+    def test_is_power_of_two_zero(self):
+        assert not bit.is_power_of_two(0)
+
+    def test_is_power_of_two_one(self):
+        assert bit.is_power_of_two(1)
+
+    def test_is_power_of_two_random_true(self):
+        for i in range(RANDOM_TEST_ITERATION):
+            random_int = random.randint(0, 999999999)
+            assert bit.is_power_of_two(2 ** random_int), \
+                'failed to identify power of 2 with power = {}'.format(random_int)
+
+    def test_is_power_of_two_random_int(self):
+        for i in range(RANDOM_TEST_ITERATION):
+            random_int = random.randint(-99999999, 99999999)
+            assert bit.is_power_of_two(random_int) == self._is_power_of_two_naive(random_int), \
+                'failed to identify random power of 2 for {}'.format(random_int)
+
+    def test_is_power_of_two_random_float(self):
+        for i in range(RANDOM_TEST_ITERATION):
+            random_float = random.random() * random.randint(-99999999, 99999999)
+            assert bit.is_power_of_two(random_float) == self._is_power_of_two_naive(random_float), \
+                'failed to identify power of 2 for {}'.format(random_float)
+
+
+class BitGetProfession(object):
+    """
+                E
+           /        \
+          E          D
+        /   \       /  \
+       E     D     D    E
+      / \   / \   / \   / \
+     E   D D   E  D  E  E  D
+    """
+
+    @staticmethod
+    def _get_profession_naive(level, position):
+        professions = {
+            (1, 1): 'E',
+            (2, 1): 'E', (2, 2): 'D',
+            (3, 1): 'E', (3, 2): 'D', (3, 3): 'D', (3, 4): 'E',
+            (4, 1): 'E', (4, 2): 'D', (4, 3): 'D', (4, 4): 'E',
+            (4, 5): 'D', (4, 6): 'E', (4, 7): 'E', (4, 8): 'D'
+        }
+
+        return professions[(level, position)]
+
+    def test_get_profession_random(self):
+
+        for i in range(RANDOM_TEST_ITERATION):
+            random_level = random.randint(1, 4)
+            random_position = random.randint(1, 2 ** (random_level - 1))
+
+            assert bit.get_profession(random_level, random_position) == self._get_profession_naive(
+                random_level, random_position), 'failed for level = {}, position = {}'.format(
+                random_level, random_position)
