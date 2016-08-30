61896a11f09f3a5691e1d7ce6d0d5b8c248524a0
stonemary <stonary.henary@gmail.com>
2016-08-29 21:11:16 -0700

    CR changes for python_decorator: 1. python functions

--- other_topics/python_decorators/section_one_functions_in_python.py
+++ other_topics/python_decorators/section_one_functions_in_python.py
@@ -1,3 +1,5 @@
+# pylint: disable=pointless-string-statement, missing-docstring, invalid-name
+
 """
 1. functions in Python.
 What you need to know about functions in Python for decorators.
@@ -5,13 +7,15 @@ What you need to know about functions in Python for decorators.
 
 
 """
-In python, functions are objects, therefore they can be used in many ways:
+In python, functions are first class citizens; they can be used in many ways.
+https://en.wikipedia.org/wiki/First-class_function
 
 1.1 functions can be assigned to variables.
 """
 
 
 def assignable_functions():
+    """ Prints hello world"""
     print('hello world')
 
 
@@ -21,11 +25,12 @@ hello_world_function()
 
 
 """
-1.2 can define inner functions
+1.2 functions can define inner functions
 """
 
 
 def outer_function(name):
+    """ A function that defines an inner function and use it. """
 
     def get_hi():
         return 'hi'
@@ -33,7 +38,7 @@ def outer_function(name):
     return get_hi() + ' ' + name
 
 
-print('1.2 can define inner functions')
+print('1.2 functions can define inner functions')
 print(outer_function('Seattle'))
 
 
@@ -43,7 +48,9 @@ print(outer_function('Seattle'))
 
 
 def get_hello_world_function():
-
+    """
+    A function that returns a function.
+    """
     def inner_hello_world_function():
         return 'hello world again, this is not fun.'
 
@@ -63,10 +70,23 @@ print(my_inner_hello_world_function())
 
 
 def capitalize(string):
+    """
+    Given a str string, returns a string with all characters in string
+    capitalized.
+    :param string: a string
+    :return: capitalized string
+    """
     return string.capitalize()
 
 
 def get_formatted_string(formatter, string):
+    """
+    Given a string formatter `formatter` and a str `string`, returns the result
+    of the formatter applied to  the string.
+    :param formatter: the formatter function which take a str as a parameter
+    :param string: a str
+    :return: the formatted string
+    """
     return formatter(string)
 
 
@@ -81,6 +101,12 @@ print(get_formatted_string(capitalize, "proudly cap'ed"))
 
 
 def old_outer_function(name):
+    """
+    Given a str name, returns a function that will return 'Hello `name`' when
+     called.
+    :param name: a str
+    :return: a function.
+    """
 
     def new_inner_function():
         return 'Hello ' + name
@@ -98,10 +124,19 @@ print(hello_name_function())
 1.5.1 Note: inner function cannot assign to outer scope
 """
 
+
 def outer_scope_function(name):
+    """
+    Given a str name, returns a function that will return 'Hello `name`' when
+     called.
+    :param name: a str
+    :return: a function.
+    """
 
     def inner_scope_function():
-        capped_name = name.capitalize()
+        _ = name.capitalize()
+        # uncomment the following line and you will have an error. Python does
+        #  not make its scope dynamically.
         # name = 'some other name'
         return 'Hello ' + name
 
