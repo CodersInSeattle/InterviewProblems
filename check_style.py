ba34f77f69b9f78ea907c4a1ba17214c5f14c40b
Kuai Yu <yukuairoy@gmail.com>
2016-09-05 06:09:06 -0700

    Improve Python style checking. Added multithreading style checking.

--- check_style.py
+++ check_style.py
@@ -1,22 +1,15 @@
-import logging
 import os
 import sys
 
 import python_style_checker
 
 
-logging.basicConfig(level=logging.INFO)
-LOGGER = logging.getLogger(__name__)
+class ImproperStyleError(Exception):
+    pass
+
 
 LINTERS = [python_style_checker.PylintRunner(),
            python_style_checker.Pep8Runner()]
-
-IGNORED_FILES = set()
-
-
-def _should_check(module):
-    module_name = os.path.basename(module)
-    return module.endswith('.py') and module_name not in IGNORED_FILES
 
 
 def _get_starting_directory(args):
@@ -28,27 +21,30 @@ def _get_starting_directory(args):
 
 
 def _run_linter(base_directory, linter):
-    LOGGER.info('***************Running %s***************', linter.linter_name)
     for root, _, files in os.walk(base_directory):
         for name in files:
             file_path = os.path.join(root, name)
-            if _should_check(file_path):
-                LOGGER.info('Checking %s', file_path)
+            if file_path.endswith('.py'):
                 linter.check_style(file_path)
-            else:
-                LOGGER.debug('Ignore %s', file_path)
-    LOGGER.info('%s:\n\tPassed: %d;\n\tFailed: %d', linter.linter_name,
-                linter.passed, linter.failed)
+    return linter.failed
+
+
+def run_linters(base_directory):
+    failed_modules = set()
+    for linter in LINTERS:
+        failed = _run_linter(base_directory, linter)
+        failed_modules.update(failed)
+    return failed_modules
 
 
 def main():
     base_directory = _get_starting_directory(sys.argv)
-    LOGGER.debug('Inspecting in directory %s...', base_directory)
-    for linter in LINTERS:
-        _run_linter(base_directory, linter)
 
-    if any(linter.failed > 0 for linter in LINTERS):
-        sys.exit()
+    failed_modules = run_linters(base_directory)
+
+    if failed_modules:
+        raise ImproperStyleError(
+            'These modules failed style checking: {}'.format(failed_modules))
 
 
 if __name__ == '__main__':
